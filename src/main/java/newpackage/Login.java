b0cd39510b277403017c9a3a924f20b2149613db
suruchishrey <57124147+suruchishrey@users.noreply.github.com>
2020-03-03 22:58:56 +0530

    Update Login.java

--- src/main/java/newpackage/Login.java
+++ src/main/java/newpackage/Login.java
@@ -3,219 +3,224 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
  *
  * @author Dell
  */
-import java.awt.Image;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
-public class Login extends JFrame {
-
-    /**
-     * Creates new form Login
-     */
-    public Login() {
-        initComponents();
-
-    }
-
-
-    //@SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
-        jLabel7 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Login");
-        setUndecorated(true);
-        setResizable(false);
-
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setLayout(null);
-
-        jPanel2.setBackground(new java.awt.Color(153, 255, 51));
-
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setIcon(new javax.swing.ImageIcon("E:\\4thSEM\\AWP\\login icon7.png")); // NOI18N
-
-        jLabel3.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("XYZ Members Login");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(55, 55, 55)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(55, 55, 55))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(122, 122, 122))
-        );
-
-        jPanel1.add(jPanel2);
-        jPanel2.setBounds(0, 0, 310, 430);
-
-        jLabel4.setFont(new java.awt.Font("Calibri", 0, 36)); // NOI18N
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setText("Login");
-        jPanel1.add(jLabel4);
-        jLabel4.setBounds(370, 30, 170, 80);
-
-        jTextField1.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
-        jTextField1.setForeground(new java.awt.Color(51, 51, 51));
-        jTextField1.setBorder(javax.swing.BorderFactory.createLineBorder(null));
-        jPanel1.add(jTextField1);
-        jTextField1.setBounds(360, 150, 220, 40);
-
-        jLabel5.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
-        jLabel5.setText("Username");
-        jPanel1.add(jLabel5);
-        jLabel5.setBounds(360, 120, 60, 20);
-
-        jLabel6.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
-        jLabel6.setText("Password");
-        jPanel1.add(jLabel6);
-        jLabel6.setBounds(360, 224, 70, 20);
-
-        jPasswordField1.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
-        jPasswordField1.setBorder(javax.swing.BorderFactory.createLineBorder(null));
-        jPanel1.add(jPasswordField1);
-        jPasswordField1.setBounds(360, 250, 220, 40);
-
-        jButton1.setBackground(new java.awt.Color(153, 255, 0));
-        jButton1.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Login");
-        jButton1.setBorder(null);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton1.setDoubleBuffered(true);
-        jButton1.setRequestFocusEnabled(false);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton1);
-        jButton1.setBounds(430, 320, 70, 30);
-
-        jLabel7.setFont(new java.awt.Font("Calibri", 0, 12)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(0, 204, 0));
-        jLabel7.setText("<html><u>Not a member? Click here to Register</html>");
-        jLabel7.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jPanel1.add(jLabel7);
-        jLabel7.setBounds(360, 360, 220, 40);
-
-        jButton2.setBackground(new java.awt.Color(153, 255, 0));
-        jButton2.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("X");
-        jButton2.setBorder(null);
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.setDoubleBuffered(true);
-        jButton2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton2MouseClicked(evt);
-            }
-        });
-        jPanel1.add(jButton2);
-        jButton2.setBounds(590, 0, 30, 30);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-
-        setSize(new java.awt.Dimension(620, 430));
-        setLocationRelativeTo(null);
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
-        // TODO add your handling code here:
-        this.dispose();
-    }//GEN-LAST:event_jButton2MouseClicked
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+public class login{
+    
+    private JFrame frame;
+    private JLabel username,password,alert;
+    private JTextField user;
+    private JPasswordField pass;
+    private JComboBox<String> choose;
+    private JButton submit,registerbtn;
+    boolean login1=false;
+    
+    public login(){
+        frame=new JFrame("Login");
+        
+        frame.setSize(500, 500);
+        frame.setLocation(400, 100);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //frame.setLocationByPlatform(true);
+        
+        
+        
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            
+        }catch(ClassNotFoundException | UnsupportedLookAndFeelException | InstantiationException | IllegalAccessException e)
+        {
+            System.err.println("Unsupported look and feel");
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Login().setVisible(true);
+        username=new JLabel("Username:");
+        password=new JLabel("Password:");
+        user=new JTextField();
+        user.setPreferredSize(new Dimension(200,30));
+        pass=new JPasswordField();
+        pass.setPreferredSize(new Dimension(200,30));
+        submit=new JButton("Submit");
+        registerbtn=new JButton("Register");
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c=new GridBagConstraints();
+        c.insets=new Insets(15,15,0,15);
+        c.gridx=0;
+        c.gridy=1;
+        c.anchor=GridBagConstraints.LINE_END;
+        panel.add(username,c);
+        c.gridx=1;
+        c.gridy=1;
+        c.anchor=GridBagConstraints.LINE_START;
+        panel.add(user,c);
+        c.gridx=0;
+        c.gridy=2;
+        c.anchor=GridBagConstraints.LINE_END;
+        panel.add(password,c);
+        c.gridx=1;
+        c.gridy=2;
+        c.anchor=GridBagConstraints.LINE_START;
+        panel.add(pass,c);
+        c.gridx=0;
+        c.gridy=4;
+        c.anchor=GridBagConstraints.CENTER;
+        c.gridwidth=2;
+        panel.add(submit,c);
+        c.gridx=1;
+        c.gridy=4;
+        c.anchor=GridBagConstraints.LINE_END;
+        panel.add(registerbtn,c);
+        frame.setResizable(false);
+        panel.setBackground(Color.white);
+        choose=new JComboBox();
+        choose.addItem("Admin");
+        choose.addItem("Student");
+        c.gridx=0;
+        c.gridy=0;
+        c.anchor=GridBagConstraints.CENTER;
+        c.gridwidth=2;
+        panel.add(choose,c);
+        alert=new JLabel();
+        c.gridx=0;
+        c.gridy=5;
+        c.anchor=GridBagConstraints.CENTER;
+        c.gridwidth=2;
+        panel.add(alert,c);
+        frame.setVisible(true);
+        
+        submit.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                int curr_user=choose.getSelectedIndex();
+                boolean b=IsNotempty();
+                if(b==TRUE)
+                {
+                    
+                    if(curr_user==1)
+                    {
+                        String username=user.getText();
+                        String pwd=pass.getText();
+                        try {
+                            Class.forName("com.mysql.jdbc.Driver");
+                        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdetails", "root", "Suruchi@2001")) {
+                            java.sql.Statement sta= con.createStatement();
+                          
+                            String query= "SELECT Name,Password FROM Users WHERE Name=? AND Password=?";
+                            PreparedStatement ps=con.prepareStatement(query);
+                            //ps.setString(1,ID);
+                            ps.setString(1, username);
+                            ps.setString(2, pwd);
+                            ResultSet rs=ps.executeQuery();
+                            if(rs.next())
+                            {
+                                String checkUser = rs.getString(1);
+                            
+                                String checkPass = rs.getString(2);
+                            
+                                if(checkUser.equals(username) && (checkPass.equals(pwd)))
+                                {
+                                    login1=TRUE;
+                                    alert.setText("Logging in!!!");
+                                    studentpanel studentpanel = new studentpanel(username,pwd);
+                                    frame.dispose();
+                                }
+                                else
+                                {
+                                    login1=FALSE;
+                                    alert.setText("Incorrect username or password!!!");
+                                }
+                            }
+                            else{
+                                alert.setText("Incorrect username or password!!!");
+                            }
+                        }  
+       
+                            
+                        } catch (ClassNotFoundException | SQLException ex) {
+                            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    else
+                    {
+                        
+                        if(choose.getSelectedIndex()==0)
+                        {
+                            alert.setText("Please login as student!!!");
+                        }
+                        else{
+                            String username=user.getText();
+                            String pwd=pass.getText();
+                            if(username.equals("admin") && pwd.equals("admin1"))
+                            {
+                                new AdminDash();
+                            }
+                        }
+                        
+                    }
+                }
+                else 
+                    {
+                        alert.setText("Please fill the required details!!!");
+                    }
+                    
+                    
+            }
+        }
+        );
+        
+        registerbtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                if(choose.getSelectedIndex()==0)
+                {
+                    alert.setText("Please register as student!!!");
+                }
+                else{
+                    new Register();
+                    frame.dispose();
+                }
+                
             }
         });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
-    // End of variables declaration//GEN-END:variables
+    
+    boolean IsNotempty()
+    {
+        String name=user.getText();
+        String pwd=pass.getText();
+        boolean retval=true;
+        if((name.length()==0) || (pwd.length()==0))
+        {
+            retval=false;
+        }
+        return retval;    
+    }
+    
+    public static void main(String args[]) {
+        login loginform = new login();
+    }
 }
